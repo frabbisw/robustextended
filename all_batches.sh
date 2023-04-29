6f83f79ac8832a10039f49e86c260166a1319e30
Fazle Rabbi <frduiit@gmail.com>
2023-04-29 15:22:48 -0400

    scripts for js

---
+++ all_batches.sh
@@ -0,0 +1,56 @@
+cd batch_files_lab
+./java_func_name_FuncRenameSnakeCase.sh
+cd batch_files_lab
+./java_func_name_FuncRenameInflectionalVariation.sh
+cd batch_files_lab
+./java_func_name_FuncRenameButterFinger.sh
+cd batch_files_lab
+./java_func_name_FuncRenameSynonymSub.sh
+cd batch_files_lab
+./java_func_name_FuncRenameChangeChar.sh
+cd batch_files_lab
+./java_func_name_FuncRenameSwapChar.sh
+cd batch_files_lab
+./java_format_new_line_aftercode.sh
+cd batch_files_lab
+./java_format_doc2comments.sh
+cd batch_files_lab
+./java_format_tab_indent.sh
+cd batch_files_lab
+./java_format_new_line_afterdoc.sh
+cd batch_files_lab
+./java_format_new_lines.sh
+cd batch_files_lab
+./java_format_split_lines.sh
+cd batch_files_lab
+./java_nlaugmenter_TenseTransformationPast.sh
+cd batch_files_lab
+./java_nlaugmenter_ChangeCharCase.sh
+cd batch_files_lab
+./java_nlaugmenter_SynonymInsertion.sh
+cd batch_files_lab
+./java_nlaugmenter_BackTranslation.sh
+cd batch_files_lab
+./java_nlaugmenter_SynonymSubstitution.sh
+cd batch_files_lab
+./java_nlaugmenter_WhitespacePerturbation.sh
+cd batch_files_lab
+./java_nlaugmenter_ButterFingersPerturbation.sh
+cd batch_files_lab
+./java_nlaugmenter_EnglishInflectionalVariation.sh
+cd batch_files_lab
+./java_nlaugmenter_TenseTransformationFuture.sh
+cd batch_files_lab
+./java_nlaugmenter_SwapCharactersPerturbation.sh
+cd batch_files_lab
+./java_natgen_ForWhileTransformer.sh
+cd batch_files_lab
+./java_natgen_OperandSwap.sh
+cd batch_files_lab
+./java_natgen_VarRenamerRN.sh
+cd batch_files_lab
+./java_natgen_DeadCodeInserter.sh
+cd batch_files_lab
+./java_natgen_VarRenamerNaive.sh
+cd batch_files_lab
+./java_natgen_VarRenamerCB.sh
---
+++ batch_files/js_nlaugmenter_WhitespacePerturbation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#$ -N js_nlaugmenter_WhitespacePerturbation
+#$ -cwd
+#$ -l m_mem_free=50G
+#$ -l g=1
+#$ -j y
+# set modules
+module load anaconda/3.2019.10/default
+module load cuda/11.4/default
+module load python/3.7.3/default
+# activate conda env
+cd /home/f_rabbi/recode/recode_multi/run_code
+source activate ReCode
+export LD_LIBRARY_PATH=/home/f_rabbi/.conda/envs/ReCode/lib/python3.8/site-packages/nvidia/cublas/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/f_rabbi/.conda/envs/conda_env/lib:$LD_LIBRARY_PATH
+# run job
+python generate_single_code_single_gpu.py ../datasets/perturbed/humanevaljs/full/nlaugmenter/humanevaljs_WhitespacePerturbation_s0.jsonl ../datasets/generated_pass5_1/js/nlaugmenter/WhitespacePerturbation
+python generate_single_code_single_gpu.py ../datasets/perturbed/humanevaljs/full/nlaugmenter/humanevaljs_WhitespacePerturbation_s3.jsonl ../datasets/generated_pass5_1/js/nlaugmenter/WhitespacePerturbation
+python generate_single_code_single_gpu.py ../datasets/perturbed/humanevaljs/full/nlaugmenter/humanevaljs_WhitespacePerturbation_s2.jsonl ../datasets/generated_pass5_1/js/nlaugmenter/WhitespacePerturbation
+python generate_single_code_single_gpu.py ../datasets/perturbed/humanevaljs/full/nlaugmenter/humanevaljs_WhitespacePerturbation_s1.jsonl ../datasets/generated_pass5_1/js/nlaugmenter/WhitespacePerturbation
+python generate_single_code_single_gpu.py ../datasets/perturbed/humanevaljs/full/nlaugmenter/humanevaljs_WhitespacePerturbation_s4.jsonl ../datasets/generated_pass5_1/js/nlaugmenter/WhitespacePerturbation
+
+# clean loaded modules
+module unload anaconda/3.2019.10/default
+module unload cuda/11.4/default
+module unload python/3.7.3/default
