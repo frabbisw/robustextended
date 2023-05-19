0de5eb9c3dadf8419a95693a0c4bda0b40474b4f
Fazle Rabbi <frduiit@gmail.com>
2023-05-19 18:20:37 -0400

    py data

---
+++ batch_files/py_nlaugmenter_ChangeCharCase.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+#$ -N py_nlaugmenter_ChangeCharCase
+#$ -cwd
+#$ -l m_mem_free=50G
+#$ -l g=1
+#$ -j y
+# set modules
+module load anaconda/3.2019.10/default
+module load cuda/11.4/default
+module load python/3.7.3/default
+# activate conda env
+cd /home/f_rabbi/recode/recode_multi/run_code
+source activate ReCode
+export LD_LIBRARY_PATH=/home/f_rabbi/.conda/envs/ReCode/lib/python3.8/site-packages/nvidia/cublas/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/f_rabbi/.conda/envs/conda_env/lib:$LD_LIBRARY_PATH
+# run job
+python generate_single_code_single_gpu.py ../datasets/perturbed/humanevalpy/full/nlaugmenter/humanevalpy_ChangeCharCase_s2.jsonl ../datasets/codegen6bmulti/generated_pass5_1/py/nlaugmenter/ChangeCharCase codegen6bmulti
+python generate_single_code_single_gpu.py ../datasets/perturbed/humanevalpy/full/nlaugmenter/humanevalpy_ChangeCharCase_s4.jsonl ../datasets/codegen6bmulti/generated_pass5_1/py/nlaugmenter/ChangeCharCase codegen6bmulti
+python generate_single_code_single_gpu.py ../datasets/perturbed/humanevalpy/full/nlaugmenter/humanevalpy_ChangeCharCase_s1.jsonl ../datasets/codegen6bmulti/generated_pass5_1/py/nlaugmenter/ChangeCharCase codegen6bmulti
+python generate_single_code_single_gpu.py ../datasets/perturbed/humanevalpy/full/nlaugmenter/humanevalpy_ChangeCharCase_s3.jsonl ../datasets/codegen6bmulti/generated_pass5_1/py/nlaugmenter/ChangeCharCase codegen6bmulti
+python generate_single_code_single_gpu.py ../datasets/perturbed/humanevalpy/full/nlaugmenter/humanevalpy_ChangeCharCase_s0.jsonl ../datasets/codegen6bmulti/generated_pass5_1/py/nlaugmenter/ChangeCharCase codegen6bmulti
+
+# clean loaded modules
+module unload anaconda/3.2019.10/default
+module unload cuda/11.4/default
+module unload python/3.7.3/default
---
+++ batch_files/py_nominal.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#$ -N cpp_nominal
+#$ -cwd
+#$ -l m_mem_free=50G
+#$ -l g=1
+#$ -j y
+# set modules
+module load anaconda/3.2019.10/default
+module load cuda/11.4/default
+module load python/3.7.3/default
+# activate conda env
+cd /home/f_rabbi/recode/recode_multi/run_code
+source activate ReCode
+export LD_LIBRARY_PATH=/home/f_rabbi/.conda/envs/ReCode/lib/python3.8/site-packages/nvidia/cublas/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/f_rabbi/.conda/envs/conda_env/lib:$LD_LIBRARY_PATH
+# run job
+python generate_single_code_single_gpu.py ../datasets/nominal/humanevalpy_nominal_f_s0.jsonl ../datasets/codegen6bmulti/generated_pass5_1/py/nominal/ codegen6bmulti
+python generate_single_code_single_gpu.py ../datasets/nominal/humanevalpy_partial_f_s0.jsonl ../datasets/codegen6bmulti/generated_pass5_1/py/partial/ codegen6bmulti
+
+# clean loaded modules
+module unload anaconda/3.2019.10/default
+module unload cuda/11.4/default
+module unload python/3.7.3/default
