8fdec400df8beb7f91f7d3488a60c606945ef304
Fazle Rabbi <frduiit@gmail.com>
2023-07-04 01:51:47 -0400

    data updated

--- evalplus/generate_inputs.py
+++ evalplus/generate_inputs.py
@@ -4,7 +4,24 @@ import jsonlines
 from tqdm import tqdm as tq
 import sys
 import pickle
+import ast
+import re
+from collections import Counter
+def identify_argument_types(func_call_string):
+    tree = ast.parse(func_call_string)
+    func_call = tree.body[0].value
 
+    if isinstance(func_call, ast.Call):
+        arguments = func_call.args
+
+        arg_types = []
+        for arg in arguments:
+            arg_type = type(eval(ast.unparse(arg)))
+            arg_types.append(arg_type)
+
+        return arg_types
+
+    return []
 def load_prompts(filename):
     prompts = []
     with open(filename, encoding="utf8") as f:
@@ -15,9 +32,21 @@ def save_prompts(filename, prompts):
     with jsonlines.open(filename, mode='w') as writer:
         for line in prompts:
             jsonlines.Writer.write(writer, line)
+def add_slashes(input_string):
+    return input_string.replace("\r","\\r").replace("\n","\\n").replace("\\","\\\\").replace("\t","\\t").replace("\"","\\\"")
+    rets = ""
+    for s in input_string:
+        if s in ["\"", "\\"]:
+            rets += "\\" + s
+        else:
+            rets += s
+    return rets
 def recurse(line, lang):
     if line is None:
         return {"java":"null","cpp":"NULL","js":"null"}[lang]
+    if isinstance(line, bool):
+        if lang in ["java", "cpp", "js"]:
+            return str(line).lower()
     if isinstance(line, list):
         converted = []
         for l in line:
@@ -29,8 +58,10 @@ def recurse(line, lang):
         if lang == "js":
             return "[" + ", ".join(converted) + "]"
     elif isinstance(line, str):
-        return f"\"{line}\""
+        return f"\"{add_slashes(line)}\""
     elif isinstance(line, int):
+        if line > 2147483646:
+            return "2147483646"
         return str(line)
     elif isinstance(line, float):
         return str(line)
@@ -74,6 +105,7 @@ def py_to_others(line, lang):
 evalplus_type = "mini"
 python_prompts = load_prompts(f"/home/frabbi/Documents/evalplus/{evalplus_type}.jsonl")
 task_id_map = {"java":"Java", "cpp":"CPP", "js":"JavaScript"}
+
 def get_input_dict(lang):
     inputs_dict = {}
     for prompt in python_prompts:
@@ -87,13 +119,101 @@ def get_input_dict(lang):
         inputs_dict[task_id] = test_cases_others
     return inputs_dict
 
-java_inputs = get_input_dict("java")
-cpp_inputs = get_input_dict("cpp")
-js_inputs = get_input_dict("js")
+def cast_list(lst, type):
+    if not isinstance(lst, list):
+        return list(lst)
+    ret_lst = []
+    for item in lst:
+        if item == None:
+            ret_lst.append(None)
+        else:
+            try:
+                casted = type(item)
+            except:
+                casted = str(item)
+            ret_lst.append(casted)
+    return ret_lst
+def detect_list_type(lst):
+    if len(lst) == 0:
+        return "empty_list"
+    for item in lst:
+        if isinstance(item, str):
+            return str
+        if isinstance(item, float):
+            return float
+        if isinstance(item, int):
+            return int
+    return "mixed"
 
+def filter_python_prompt(prompt):
+    test_cases_py = prompt['plus_input']
+    case_types = {}
+    for i in range(len(test_cases_py)):
+        tc = test_cases_py[i]
+        for j in range(len(tc)):
+            sample = tc[j]
+            if j not in case_types.keys():
+                case_types[j] = []
+            case_type = eval(type(sample).__name__)
+            # if case_type == list:
+            #     case_type = detect_list_type(sample)
+            case_types[j].append(case_type)
+
+    for j in case_types.keys():
+        if 0 == case_types[j].count(list):
+            continue
+        if 0 < case_types[j].count(list) < len(case_types[j]):
+            for i in range(len(test_cases_py)):
+                test_cases_py[i][j] = list(test_cases_py[i][j])
+        list_types = [detect_list_type(test_cases_py[i][j]) for i in range(len(test_cases_py))]
+        if list_types.count(int) + list_types.count(float) + list_types.count("empty_list") == len(case_types[j]):
+            if list_types.count(float) > list_types.count(int):
+                for i in range(len(test_cases_py)):
+                    test_cases_py[i][j] = cast_list(test_cases_py[i][j],float)
+            else:
+                for i in range(len(test_cases_py)):
+                    test_cases_py[i][j] = cast_list(test_cases_py[i][j],int)
+            # print(list_types.count(int), list_types.count(float), list_types.count("empty_list"), len(case_types[j]))
+
+    # print(prompt['plus_input'])
+    prompt['plus_input'] = test_cases_py
+
+# for prompt in python_prompts:
+    # if prompt["task_id"] == "HumanEval/2":
+    # if prompt["task_id"] == "HumanEval/20":
+        # print(prompt['plus_input'])
+    # if prompt["task_id"] == "HumanEval/20":
+    # filter_python_prompt(prompt)
+    # if prompt["task_id"] == "HumanEval/20":
+    #     print(prompt["plus_input"])
+# print(prompt['plus_input'])
+    # if prompt["task_id"] == "HumanEval/20":
+    #     prompt["plus_input"]
+
+def filter_inputs():
+    for prompt in python_prompts:
+        # if prompt["task_id"] == "HumanEval/30":
+        filter_python_prompt(prompt)
+filter_inputs()
+java_inputs = get_input_dict("java")
 with open(f"java_inputs_{evalplus_type}", "wb") as f:
     pickle.dump(java_inputs, f)
-with open(f"cpp_inputs_{evalplus_type}", "wb") as f:
-    pickle.dump(cpp_inputs, f)
-with open(f"js_inputs_{evalplus_type}", "wb") as f:
-    pickle.dump(js_inputs, f)
+
+
+    # print(len(types[key_with_max_values]), types[key_with_max_values])
+        # print(len(types[key_with_max_values]))
+# filter_inputs("java")
+
+# task_id = "HumanEval/8"
+# for prompt in python_prompts:
+#     if prompt["task_id"] == task_id:
+#         print(prompt["plus_input"])
+
+# print(java_inputs)
+# cpp_inputs = get_input_dict("cpp")
+# js_inputs = get_input_dict("js")
+
+# with open(f"cpp_inputs_{evalplus_type}", "wb") as f:
+#     pickle.dump(cpp_inputs, f)
+# with open(f"js_inputs_{evalplus_type}", "wb") as f:
+#     pickle.dump(js_inputs, f)
--- evalplus/generate_outputs.py
+++ evalplus/generate_outputs.py
@@ -5,6 +5,7 @@ from tqdm import tqdm as tq
 import sys
 import pickle
 import subprocess
+import re
 
 def load_prompts(filename):
     prompts = []
@@ -13,11 +14,12 @@ def load_prompts(filename):
             prompts.append(json.loads(line))
     return prompts
 
-with open("java_inputs", "rb") as f:
+evalplus_type = "mini"
+with open(f"java_inputs_{evalplus_type}", "rb") as f:
     java_inputs = pickle.load(f)
-with open("cpp_inputs", "rb") as f:
+with open(f"cpp_inputs_{evalplus_type}", "rb") as f:
     cpp_inputs = pickle.load(f)
-with open("js_inputs", "rb") as f:
+with open(f"js_inputs_{evalplus_type}", "rb") as f:
     js_inputs = pickle.load(f)
 
 
@@ -25,9 +27,11 @@ java_prompts = load_prompts("../datasets/nominal/humanevaljava_nominal_f_s0.json
 cpp_prompts = load_prompts("../datasets/nominal/humanevalcpp_nominal_f_s0.jsonl")
 js_prompts = load_prompts("../datasets/nominal/humanevaljs_nominal_f_s0.jsonl")
 
+with open("java_create_map.txt", "r") as f:
+    create_map_string = f.read()
 
-def run_java(code, entry_point, task_inputs):
-    main = "public class Main {\n\tpublic static void main(String[] args) {\n\t\tSolution s = new Solution();\n"
+def run_java(code, entry_point, task_inputs, orgmain, task_id):
+    main = "public class Main {\n" + create_map_string +"\tpublic static void main(String[] args) {\n\t\tSolution s = new Solution();\n"
     for i in task_inputs:
         main += f"\t\tSystem.out.print(s.{entry_point}({i}));\n"
         main += f"\t\tSystem.out.print(\"#\");\n"
@@ -51,19 +55,53 @@ def run_java(code, entry_point, task_inputs):
         compilation_output = subprocess.run(['javac', 'Main.java', 'Solution.java'], timeout=4, capture_output=True)
         output = subprocess.run(['java', 'Main'], timeout=4, capture_output=True)
         # print("*"*50)
-        print(output.stdout.decode())
+        # print(output.stdout.decode())
+        if "error" in compilation_output.stderr.decode():
+            print(task_id)
+            print(compilation_output.stderr.decode())
+            print(repr(task_inputs))
+            print(code)
+            print(main)
+            print(repr(orgmain))
+            print("*"*50)
+            exit()
+
         # print("*" * 50)
     except Exception as e:
         print(e)
     # print(code)
     # print(main)
+
+def run_cpp(code, entry_point, task_inputs):
+    main = "#undef NDEBUG\n#include<assert.h>\n#include <iostream>\nusing namespace std;\nint main(){\n"
+    for i in task_inputs:
+        main += f"\tcout << {entry_point}({i});\n"
+        main += f"\tcout << \"#\";\n"
+    main += "}"
+    full_code = code +"\n"+ main
+    with open("../testing_dir/cpp_code.cpp", "w") as f:
+        f.write(full_code)
+    os.chdir("../testing_dir/")
+    try:
+        compilation_output = subprocess.run(['g++', '-o', 'cpp_code', 'cpp_code.cpp', '-lcrypto', '-lssl'], timeout=10, capture_output=True)
+
+        # exit()
+        output = subprocess.run(['./cpp_code'], timeout=4, capture_output=True)
+        print(output.stdout.decode())
+        print(compilation_output.stderr.decode(), output.stderr.decode())
+
+    except Exception as e:
+        print(e)
+
 def generate_output(prompts, inputs, lang):
     for prompt in prompts:
         task_id = prompt["task_id"]
         task_inputs = inputs[task_id]
         full_code = prompt["prompt"] + prompt["canonical_solution"]
         if lang == "java":
-            run_java(full_code, prompt["entry_point"], task_inputs)
+            run_java(full_code, prompt["entry_point"], task_inputs, prompt["test"], task_id)
+        elif lang == "cpp":
+            run_cpp(full_code, prompt["entry_point"], task_inputs)
 
 
 # prompt = java_prompts[0]
@@ -71,6 +109,7 @@ def generate_output(prompts, inputs, lang):
 # lang = "java"
 
 generate_output(java_prompts, java_inputs, "java")
+# generate_output(cpp_prompts, cpp_inputs, "cpp")
 
 
 # def generate_test_(lang):
