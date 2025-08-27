d78fd8898fe18fac2fbe637588551b29cf0651fc
Fazle Rabbi <frduiit@gmail.com>
2025-08-27 14:30:40 -0400

    Create check_model.py

---
+++ exp/check_model.py
@@ -0,0 +1,31 @@
+#for the code generation model
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import GPTJForCausalLM
+import torch
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+#models_dict = {"codegen6bmulti": "Salesforce/codegen-6B-multi", "codegen2bmulti": "Salesforce/codegen-2B-multi", "incoder1b": "facebook/incoder-1B", "incoder6b": "facebook/incoder-6B", "magicoder7b": "ise-uiuc/Magicoder-S-DS-6.7B"}
+models_dict = {"codegen6bmulti": "/home/f_rabbi/models/codegen-6B-multi", "magicoder7b": "/home/f_rabbi/code_trans/models/Magicoder-S-DS-6.7B"}
+
+# checkpoint = models_dict[sys.argv[1]]
+checkpoint = models_dict["codegen6bmulti"]
+
+code_generaton_model = AutoModelForCausalLM.from_pretrained(checkpoint, torch_dtype=torch.bfloat16).to(device)
+code_generaton_tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+# device_map = infer_auto_device_map(model, no_split_module_classes=["OPTDecoderLayer"])
+
+def prompt_to_code(prompt):
+    completion = code_generaton_model.generate(**code_generaton_tokenizer(prompt, return_tensors="pt").to(device), max_length=1536,temperature=0.2,top_p=0.95,do_sample = True)
+    code = code_generaton_tokenizer.decode(completion[0])
+    return code
+
+prompt = '''
+translate this following Python code to C++
+
+def sum_two(num1, num2):
+  return num1+num2
+'''
+
+cpp_code = prompt_to_code(prompt)
+
+print(cpp_code)
