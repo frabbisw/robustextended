8eeab422f36e5ec79e0f812b22eb4fbd8db8adf8
Fazle Rabbi <frduiit@gmail.com>
2025-09-21 18:56:55 -0400

    Create batch_preprocess_java_magicoder.sh

---
+++ case_study/batch_preprocess_java_magicoder.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -J java_mag_pre_doc
+#SBATCH -n4
+#SBATCH --mem=10GB
+#SBATCH --gpus=1
+#SBATCH --time=24:00:00
+#SBATCH -o _%x%J.out
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=osdefr@gmail.com
+
+source /etc/profile.d/modules.sh
+
+module load java/17.0.2
+module load gcc/11.5
+module load go/1.24.5
+module load python/3.11.6
+module load cuda/12.3.2
+module load anaconda/3.2024.10.1
+
+eval "$(conda shell.bash hook)"
+
+conda activate code_trans
+
+python preprocess_docstring.py /home/f_rabbi/recode/robustextended/datasets/samples/magicoder7b/java/nlaugmenter/sample_368.jsonl magicoder7b
