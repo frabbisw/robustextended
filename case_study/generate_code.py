22392bc0b85c6b6d4f14f551c11e49d209c44e8f
Fazle Rabbi <frduiit@gmail.com>
2025-09-22 04:53:04 -0400

    Create generate_code.py

---
+++ case_study/generate_code.py
@@ -0,0 +1,44 @@
+import json
+import os
+import jsonlines
+from tqdm import tqdm as tq
+import sys
+#for the code generation model
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import GPTJForCausalLM
+import torch
+
+def load_prompts(filename):
+    prompts = []
+    with open(filename, encoding="utf8") as f:
+        for line in f.readlines():
+            prompts.append(json.loads(line))
+    return prompts
+def save_prompts(filename, prompts):
+    with jsonlines.open(filename, mode='w') as writer:
+        for line in prompts:
+            jsonlines.Writer.write(writer, line)
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+#models_dict = {"codegen6bmulti": "Salesforce/codegen-6B-multi", "codegen2bmulti": "Salesforce/codegen-2B-multi", "incoder1b": "facebook/incoder-1B", "incoder6b": "facebook/incoder-6B", "magicoder7b": "ise-uiuc/Magicoder-S-DS-6.7B"}
+models_dict = {"magicoder7b": "/home/f_rabbi/models/Magicoder-S-DS-6.7B", "qwencoder": "/home/f_rabbi/models/Qwen2.5-Coder-7B-Instruct", "codegen6bmulti": "/home/f_rabbi/models/codegen-6B-multi"}
+
+checkpoint = models_dict[sys.argv[2]]
+
+code_generaton_model = AutoModelForCausalLM.from_pretrained(checkpoint, torch_dtype=torch.bfloat16).to(device)
+code_generaton_tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+# device_map = infer_auto_device_map(model, no_split_module_classes=["OPTDecoderLayer"])
+
+def prompt_to_code(prompt):
+    completion = code_generaton_model.generate(**code_generaton_tokenizer(prompt, return_tensors="pt").to(device), max_length=1024,temperature=0.2,top_p=0.95,do_sample = True)
+    code = code_generaton_tokenizer.decode(completion[0])
+    return code
+
+prompts = load_prompts(sys.argv[1])
+
+for i in tq(range(len(prompts))):
+    p = prompts[i]
+    p["processed_gc"] = prompt_to_code(p["processed_prompt"])
+    prompts[i] = p
+save_prompts(outpath, prompts)
+print("saved", outpath)
