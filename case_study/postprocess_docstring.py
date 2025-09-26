764b929a44cab03cc476cfddc3c6b52a9f473c80
Fazle Rabbi <frduiit@gmail.com>
2025-09-26 01:42:45 -0400

    Update postprocess_docstring.py

--- case_study/postprocess_docstring.py
+++ case_study/postprocess_docstring.py
@@ -63,8 +63,8 @@ for i, prompt in enumerate(prompts):
     processed_nl = filter_gc(prompt["processed_nl"])
     if len(processed_nl) < 10:
         processed_nl = prompt["processed_nl"]
-    print(prompt["processed_nl"])
-    print("--"*50)
+    # print(prompt["processed_nl"])
+    # print("--"*50)
     print(processed_nl)
     print("=="*50)
     print()
