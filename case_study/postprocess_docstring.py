28f07a6d21cc76257f5aab030d225111f4b4e8f5
Fazle Rabbi <frduiit@gmail.com>
2025-09-30 11:40:03 -0400

    Update postprocess_docstring.py

--- case_study/postprocess_docstring.py
+++ case_study/postprocess_docstring.py
@@ -26,7 +26,7 @@ prompts = load_prompts(filepath)
 def remove_code_snippets(nl):
     lines = nl.split("\n")
     # lines = [line.strip() for line in lines if line.strip() != ""] + ["\n"] 
-    lines = [line for line in lines if "here is" not in line.lower() or "python" not in line.lower()] + ["\n"] 
+    lines = [line.strip() for line in lines if "here is a" not in line.lower() and "here is an" not in line.lower()] + ["\n"] 
     # lines = [line.strip() for line in lines] + ["\n"] 
     
     if "```" not in nl:
