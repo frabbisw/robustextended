d4f5b355c9e8820dd5ed0a6c9db4c0183a31dda5
Fazle Rabbi <frduiit@gmail.com>
2025-10-01 13:57:08 -0400

    Update and rename calc_bleu.py to calc_bleu_2_jsonl.py

--- case_study/calc_bleu.py
+++
@@ -1,6 +0,0 @@
-import sacrebleu
-
-def bleu(hypothesis, reference):
-    return sacrebleu.sentence_bleu(hypothesis, [reference]).score
-
-print(bleu("this is a test", "this is a test"))
---
+++ case_study/calc_bleu_2_jsonl.py
@@ -0,0 +1,26 @@
+import sacrebleu
+import sys
+
+jsonl_path = sys.argv[1]
+
+def load_prompts(filename):
+    prompts = []
+    with open(filename, encoding="utf8") as f:
+        for line in f.readlines():
+            prompts.append(json.loads(line))
+    return prompts
+def save_prompts(filename, prompts):
+    with jsonlines.open(filename, mode='w') as writer:
+        for line in prompts:
+            jsonlines.Writer.write(writer, line)
+
+def bleu(hypothesis, reference):
+    return sacrebleu.sentence_bleu(hypothesis, [reference]).score
+
+prompts = load_prompts(filepath)
+bleu_scores = []
+
+for prompt in prompts:
+    bleu_scores.append(bleu(prompt["nl"], prompt["filtered_nl"]))
+
+print(sum(bleu_scores)/len(bleu_scores))
