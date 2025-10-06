a125d79dde71cda56db1f5eb03b0b6b4c4ddd42f
Fazle Rabbi <frduiit@gmail.com>
2025-10-05 22:27:21 -0400

    Create batch_java_magicoder.py

---
+++ case_study/batch_java_magicoder.py
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH -J java_mag_gen
+#SBATCH -n4
+#SBATCH --mem=10GB
+#SBATCH --gpus=1
+#SBATCH --time=24:00:00
+#SBATCH -o _%x%J.out
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=osdefr@gmail.com
+
+source /etc/profile.d/modules.sh
+
+module load java/17.0.2
+module load gcc/11.5
+module load go/1.24.5
+module load python/3.11.6
+module load cuda/12.3.2
+module load anaconda/3.2024.10.1
+
+eval "$(conda shell.bash hook)"
+
+conda activate code_trans
+
+rm -r ../datasets/samples/
+python sampling.py magicoder7b java nlaugmenter
+python preprocess_docstring.py /home/f_rabbi/recode/robustextended/datasets/samples/magicoder7b/java/nlaugmenter/sample_368.jsonl magicoder7b
+python postprocess_docstring.py magicoder7b java nlaugmenter > process_logs_java.txt
+python generate_code.py /home/f_rabbi/recode/robustextended/datasets/samples/magicoder7b/java/nlaugmenter/sample_368.jsonl magicoder7b
+python prepare_orgs.py ../datasets/samples/magicoder7b/java/nlaugmenter/sample_368.jsonl ../datasets/magicoder7b/generated_pass5_1/java/nominal/f_s0.jsonl ../datasets/samples/magicoder7b/java/nlaugmenter/sample_368_org.jsonl
+echo "original correct items"
+python test_single_old.py ../datasets/samples/magicoder7b/java/nlaugmenter/sample_368_org.jsonl java ../datasets/samples/magicoder7b/java/nlaugmenter/sample_368_org.jsonl 2
+echo "perturbed correct items"
+python test_single_old.py ../datasets/samples/magicoder7b/java/nlaugmenter/sample_368.jsonl java ../datasets/samples/magicoder7b/java/nlaugmenter/sample_368.jsonl 2
+echo "processed correct items"
+python test_single.py ../datasets/samples/magicoder7b/java/nlaugmenter/sample_368.jsonl java ../datasets/samples/magicoder7b/java/nlaugmenter/sample_368_r.jsonl 2
+echo "Done!"
