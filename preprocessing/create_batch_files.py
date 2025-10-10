6a2f24027ab8b12ec0556001426d581554bd4008
Fazle Rabbi <frduiit@gmail.com>
2025-10-10 01:56:46 -0400

    Update create_batch_files.py

--- preprocessing/create_batch_files.py
+++ preprocessing/create_batch_files.py
@@ -39,7 +39,7 @@ python parse_nl.py {file_path} {lang}
 python pre_docstring.py {file_path} magicoder7b
 python post_docstring.py {file_path} {lang}
 python generate_code.py {file_path} magicoder7b
-python test_single.py {file_path} {lang} {5 + int(part)}
+python test_single.py {file_path} {lang} {int(part)}
 echo "done {file_path}"
     '''
 
