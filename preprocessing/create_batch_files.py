4497be52a7361246ddebae7a4f329c0966c850a7
Fazle Rabbi <frduiit@gmail.com>
2025-10-10 01:51:15 -0400

    Update create_batch_files.py

--- preprocessing/create_batch_files.py
+++ preprocessing/create_batch_files.py
@@ -17,6 +17,7 @@ module load go/1.24.5
 module load python/3.11.6
 module load cuda/12.3.2
 module load anaconda/3.2024.10.1
+export PATH="$HOME/my-nodejs/bin:$PATH"
 
 eval "$(conda shell.bash hook)"
 
