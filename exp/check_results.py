1e7828f0520cae0e52b20296f0f6a8a55b40fd81
Fazle Rabbi <frduiit@gmail.com>
2025-10-13 20:56:50 -0400

    Update check_results.py

--- exp/check_results.py
+++ exp/check_results.py
@@ -374,8 +374,9 @@ def prepare_overleaf_table_summary(model_dict):
                     if method_map[method] not in method_dict.keys():
                         method_dict[method_map[method]] = {"rpk":[], "rdk":[], "rrk":[]}
                 except Exception as e:
-                    print(method)
                     print(e)
+                    print("-"*50)
+                    print(method)
                     exit()
                 passatk_worst, robust_drop, robust_relative = lang_dict[method]
                 method_dict[method_map[method]]["rpk"].append(passatk_worst)
