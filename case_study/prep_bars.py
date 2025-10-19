06b9919dca056da71413ebed9ec3473d5d830680
Fazle Rabbi <frduiit@gmail.com>
2025-10-19 02:50:11 -0400

    Create prep_bars.py

---
+++ case_study/prep_bars.py
@@ -0,0 +1,30 @@
+import json
+import pdb
+import pickle
+from tqdm import tqdm as tq
+import os
+import subprocess
+import jsonlines
+from collections import Counter
+from os import listdir
+from os.path import isfile, join
+import sys
+
+CODE_RUN_STATUS = {"PASSED":0, "ASSERTION":1, "COMPILATION":2, "TIMEOUT": 3, "RUNTIME": 4}
+
+def load_prompts(filename):
+    prompts = []
+    with open(filename, encoding="utf8") as f:
+        for line in f.readlines():
+            prompts.append(json.loads(line))
+    return prompts
+def save_prompts(filename, prompts):
+    # print(prompts)
+    # exit()
+    with jsonlines.open(filename, mode='w') as writer:
+        for line in prompts:
+            jsonlines.Writer.write(writer, line)
+
+lang = sys.argv[1]
+model = "magicoder7b"
+
