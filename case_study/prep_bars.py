a8ec78ac806a0fb3961a66791d66fe624083d69c
Fazle Rabbi <frduiit@gmail.com>
2025-10-19 05:58:43 -0400

    Update prep_bars.py

--- case_study/prep_bars.py
+++ case_study/prep_bars.py
@@ -25,6 +25,30 @@ def save_prompts(filename, prompts):
         for line in prompts:
             jsonlines.Writer.write(writer, line)
 
+DATASET_PATH = "/home/f_rabbi/recode/robustextended/datasets"
+
+def get_nominal_map(lang, type, model_name):
+    if type == "nominal":
+        prompts = load_prompts(f"{DATASET_PATH}/{model_name}/generated_pass5_1/{lang}/nominal/f_s0.jsonl")
+    elif type == "partial":
+        prompts = load_prompts(f"{DATASET_PATH}/{model_name}/generated_pass5_1/{lang}/partial/f_s0.jsonl")
+    
+    return {p["task_id"]: p for p in prompts}
+        
 lang = sys.argv[1]
 model = "magicoder7b"
+pert_type = "nlaugmenter"
 
+nominal_map = (lang, "nominal", model_name)
+
+stat = {}
+for aug_type in os.listdir(f"{DATASET_PATH}/{model_name}/generated_pass5_1/{lang}/{pert_type}"):
+    stat[aug_type] = {"nominal": 0, "perturbed": 0, "fixed": 0}
+    for i in range(5):
+        aug_filepath = f"{DATASET_PATH}/{model_name}/backup/{lang}/{pert_type}/{aug_type}/f_s{i}.jsonl"
+        pert_prompts = load_prompts(aug_filepath)
+        for i, p in enumerate(pert_prompts):
+            stat[aug_type]["nominal"] += int(nominal_map[pert_prompts[i]["task_id"]]["passed_evalplus"])
+            stat[aug_type]["perturbed"] += int(pert_prompts[i]["passed_evalplus"])
+            stat[aug_type]["perturbed"] += int(pert_prompts[i]["passed_evalplus_processed"])
+        
