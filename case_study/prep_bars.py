91734d4477474297ac1f050cd1d7aca9adba6a16
Fazle Rabbi <frduiit@gmail.com>
2025-10-20 00:02:00 -0400

    Update prep_bars.py

--- case_study/prep_bars.py
+++ case_study/prep_bars.py
@@ -55,7 +55,7 @@ for aug_type in os.listdir(f"{DATASET_PATH}/{model_name}/generated_pass5_1/{lang
     stat[aug_type] = {"nominal": 0, "perturbed": 0, "fixed": 0}
     for ind in range(5):
         aug_filepath = f"{DATASET_PATH}/{model_name}/backup/{lang}/{pert_type}/{aug_type}/f_s{ind}.jsonl"
-        if not os.path.exist(aug_filepath):
+        if not os.path.exists(aug_filepath):
             print(f"not exists: {aug_filepath}")
             continue
         pert_prompts = load_prompts(aug_filepath)
