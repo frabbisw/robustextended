1d3390748f0980eaa5fdd8a3d124da0b5a29a58d
Fazle Rabbi <frduiit@gmail.com>
2025-10-29 04:20:14 -0400

    Update robust_heatmap.py

--- rqs/robust_heatmap.py
+++ rqs/robust_heatmap.py
@@ -117,12 +117,11 @@ def get_df(df, lang, pert_type):
 with open("data_with_robust.pkl", "rb") as f:
 	data_with_robust = pickle.load(f)
 
-print(columns)
-
 columns = data_with_robust[0]
 rows = data_with_robust[1:]
 df = pd.DataFrame(rows, columns=columns)
 
+print(columns)
 print(df.columns)
 
 print()
