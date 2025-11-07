7096185a55a1c1111f45fc60b90546d7c87594d4
Fazle Rabbi <frduiit@gmail.com>
2025-11-07 00:56:44 -0500

    Update process_annot_2.py

--- annotations/process_annot_2.py
+++ annotations/process_annot_2.py
@@ -8,6 +8,7 @@ def convert(lang):
     groups = contents.split(stop_token)
     for group in groups:
         task_id, _, _, _, nom, pert, sem, _  = group.split("-"*22)
+        task_id = task_id.split(":")[-1].strip()
         # nom = nom.split(":")[-1].strip()
         # pert = pert.split(":")[-1].strip()
         # sem = sem.split(":")[-1].strip()                
@@ -15,7 +16,7 @@ def convert(lang):
         nom = float(nom.split(":")[-1].strip())
         sem = float(sem.split(":")[-1].strip())
         # print(pert, nom, sem)
-        updated_groups.append(f"{unique_id}\nPerturbed prompt:{pert}\nNominal prompt:{nom}\nSemantic:{sem}")
+        updated_groups.append(f"{task_id}\nPerturbed prompt:{pert}\nNominal prompt:{nom}\nSemantic:{sem}")
 
     with open(f"{lang}_2.txt", "w") as f:
         f.write("\n\n".join(updated_groups))
