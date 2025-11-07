6349fb9d90a3e6626db4c7ac6a6e75f4ee1e37bd
Fazle Rabbi <frduiit@gmail.com>
2025-11-07 01:13:26 -0500

    Update annot.py

--- annotations/annot.py
+++ annotations/annot.py
@@ -42,6 +42,9 @@ def evaluate_annotations(lang):
     
     # Sanity check: both must have the same number of samples
     if len(annotator1) != len(annotator2):
+        print(len(annotator1), len(annotator2))
+        print(annotator1[-1], len(annotator2)[-1])
+        
         raise ValueError("Annotator files have different number of samples")
     
     keys = ['naturalness_nominal', 'naturalness_perturbed', 'similarity']
